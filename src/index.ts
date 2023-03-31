8bc959816fe7151e641f1af964ef5dc1ee0241cb
EtienneDuv <etienne.duverney@gmail.com>
2023-03-31 18:21:54 +0200

    [Add] HTML preview

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,30 @@
-import Quill from 'quill'
+import Quill from 'quill';
 
-new Quill('#quill-editor', {
+const quill = new Quill('#editor', {
   modules: {
     toolbar: [
-      [{ header: [1, 2, false] }],
+      [{header: [1, 2, false]}],
       ['bold', 'italic', 'underline'],
       ['image', 'code-block']
     ]
   },
-  placeholder: 'Compose an epic...',
   theme: 'snow'
+});
+
+quill.on('text-change', (delta, oldDelta, source) => {
+  if (source == 'api') console.log('An API call triggered this change.');
+  if (source == 'user') {
+    const editorContent = document.getElementById('editor')?.getElementsByClassName('ql-editor')[0]?.outerHTML;
+    const targetElement = document.getElementById('editor-html');
+
+    const cleanedContent = editorContent
+      ?.replace(/(?<start><img src="data:image.*?base64,)[^"]+"/g, '$<start>...')
+      .replace(/<p><br><\/p>/g, '~~~~~')
+      .replace(/<p>(?<content>.+?)<\/p>/g, '<p>$<content></p>\n')
+      .replace(/~~~~~/g, '<p><br></p>\n');
+
+    if (targetElement && cleanedContent) {
+      targetElement.textContent = cleanedContent;
+    }
+  }
 });
